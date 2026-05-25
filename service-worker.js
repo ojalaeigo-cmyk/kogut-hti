fff0f0e4e8e1a903d3a9a6a29ef73213b3f24f31
ojalaeigo-cmyk <ojalaeigo@gmail.com>
2026-05-25 03:22:22 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='star-king-stabiilne-uus-loogika-v1';
+const CACHE='star-king-uus-loogika-avakuva-clean-v1';
 const ASSETS=['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));});
